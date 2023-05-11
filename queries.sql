8a4556d3524290b49429028d210a1771aa83e268
Chere Lemma <chere.lemma@aastu.edu.et>
2023-05-11 23:07:59 +0300

    Add Transaction Queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,30 @@ SELECT name, escape_attempts FROM ANIMALS WHERE weight_kg > 10.5
 SELECT * FROM ANIMALS WHERE neutered = true
 SELECT * FROM ANIMALS WHERE name != 'Gabumon'
 SELECT * FROM ANIMALS WHERE weight_kg BETWEEN 10.4 AND 17.3
+
+BEGIN;
+UPDATE ANIMALS SET species = 'unspecified';
+ROLLBACK;
+
+UPDATE ANIMALS SET species = 'digimon' WHERE name LIKE '%mon'
+UPDATE ANIMALS SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN TRANSACTION;
+DELETE FROM animals
+SELECT * from animals
+ROLLBACK
+SELECT * from animals
+
+
+BEGIN TRANSACTION;
+DELETE FROM animals where date_of_birth > '1/1/2022'
+SAVEPOINT sp1
+UPDATE ANIMALS SET weight_kg =  weight_kg * -1
+SELECT * from animals
+ROLLBACK
+SELECT * from animals
+
+UPDATE ANIMALS SET weight_kg =  weight_kg * -1 where weight_kg < 0
+COMMIT
+SELECT * from animals
