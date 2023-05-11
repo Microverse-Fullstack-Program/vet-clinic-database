5d275e4d8a60823250b8fd24e40c4a768ecd6dc9
Chere Lemma <chere.lemma@aastu.edu.et>
2023-05-12 01:45:04 +0300

    Inser owners and species records

--- data.sql
+++ data.sql
@@ -9,3 +9,19 @@ INSERT INTO ANIMALS (NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG)
 VALUES ('Charmander', '2/8/2020', 0, false, 11), ('Plantmon', '11/15/2021', 2, true, -5.7), ('Squirtle', '4/2/1993', 3, false, -12.13),
 ('Angemon', '6/12/2005', 0, true, -45), ('Boarmon', '6/7/2005', 7, true, 20.4), ('Blossom', '10/13/1998', 3, true, 17), ('Ditto', '5/14/2022', 4, true, 22)
 
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38)
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon')
+COMMIT
+  
+UPDATE animals SET species_id = CASE WHEN name ILIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon') 
+   ELSE (SELECT id FROM species WHERE name = 'Pokemon') END WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 
+	CASE 
+		WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+		WHEN name IN ('Gabumon', 'Pikachu')THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+		WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+		WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond') 
+		ELSE (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+	END
+COMMIT
